a04560f8e0f9a3766e0e76053f387c627641925f
ohs80340 <ohs80340@ohs80340noMacBook-Air.local>
2021-01-24 12:46:45 +0900

    configure

--- src/http_server/getting_started_server_1.rs
+++ src/http_server/getting_started_server_1.rs
@@ -1,25 +1,27 @@
-use actix_web::{ web };
+use actix_web::{ web, HttpResponse };
 
-// HttpServerは、アプリケーションインスタンスではなく、アプリケーションファクトリを受け入れます。 
-// HttpServerは、スレッドごとにアプリケーションインスタンスを構築します。
-// したがって、アプリケーションデータは複数回作成する必要があります。
-// 異なるスレッド間でデータを共有する場合は、共有可能なオブジェクトを使用する必要があります。送信+同期。
+// この関数は別のモジュールに配置できます
+pub fn scoped_config(cfg: &mut web::ServiceConfig) {
+     cfg.service(
+         web::resource("/test")
+         .route(web::get().to(|| HttpResponse::Ok().body("test")))
+         .route(web::head().to(|| HttpResponse::MethodNotAllowed())),
+     );
+ }
+ 
+ 
+ 
+ // この関数は別のモジュールに配置できます
+ pub fn config(cfg: &mut web::ServiceConfig) {
+     cfg.service(
+         web::resource("/app")
+         .route(web::get().to(|| HttpResponse::Ok().body("app")))
+         .route(web::head().to(|| HttpResponse::MethodNotAllowed())),
+     );
+ }
 
-// 内部的には、web :: DataはArcを使用します。
-// したがって、2つのアークの作成を回避するには、App :: app_data（）を使用してデータを登録する前にデータを作成する必要があります。
+ // 上記の例の結果は次のようになります。
 
-// 次の例では、変更可能な共有状態のアプリケーションを作成します。
-// まず、状態を定義し、ハンドラーを作成します。
-
-use std::sync::Mutex;
-
-pub struct AppStateWithCounter {
-     pub counter: Mutex<i32>,      // スレッド間で安全にミューテーションするにはミューテックスが必要です
-}
-
-pub async fn index(data: web::Data<AppStateWithCounter>) -> String {
-     let mut counter = data.counter.lock().unwrap();        // <- counterのMutexGuardを取得します
-     *counter += 1;                                         // <- MutexGuard内のアクセスカウンター
-
-     format!("Request number: {}", counter)                 // <- response with count
-}
+//  /         -> "/"
+//  /app      -> "app"
+//  /api/test -> "test"
