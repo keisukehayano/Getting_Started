03035b9ce56cc519b13358cbf61616e01de7aed1
ohs80340 <ohs80340@ohs80340noMacBook-Air.local>
2021-01-24 12:17:07 +0900

    guard

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,24 @@
-use actix_web::{ web, App, HttpServer };
-use std::sync::Mutex;
+use actix_web::{ web, HttpResponse, HttpServer, guard, App };
 
 
-// web :: scope（）メソッドを使用すると、リソースグループのプレフィックスを設定できます。
-// このスコープは、リソース構成によって追加されたすべてのリソースパターンの前に付加されるリソースプレフィックスを表します。
-// これは、同じリソース名を維持しながら、元の作成者が意図したものとは異なる場所に一連のルートをマウントするのに役立ちます。
-
-// For example:
 
 #[actix_web::main]
-async fn main() {
-    let scope = web::scope("/users").service(show_users);
-    App::new().service(scope);
+async fn main() -> std::io::Result<()> {
+    HttpServer::new( || {
+        App::new()
+            .service(
+                web::scope("/")
+                .guard(guard::Header("Host", "www.rust-lang.org"))
+                .route("", web::to(|| HttpResponse::Ok().body("www"))),
+            )
+            .service(
+                web::scope("/")
+                .guard(guard::Header("Host", "www.rust-lang.org"))
+                .route("", web::to(|| HttpResponse::Ok().body("user")))
+            )
+            .route("/", web::to(|| HttpResponse::Ok()))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
-
-
-// 上記の例では、アプリケーションのスコープ引数がパターンの前に付加されるため、
-// show_usersルートの有効なルートパターンは/ showではなく/ users / showになります。
-// この場合、ルートはURLパスが/ users / showの場合にのみ一致し、
-// HttpRequest.url_for（）関数がルート名show_usersで呼び出されると、同じパスのURLが生成されます。
